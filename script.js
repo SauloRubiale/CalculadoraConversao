62b0330dddc80f03684e90423c06d3092d58b817
SauloRubiale <sauloarantesdebrito@gmail.com>
2026-03-18 09:52:58 -0300

    feat: adiciona conversão de dólar e melhora validação

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ const euro = 6;
 const dolar = 5.2;
 
 converterInput.addEventListener("click", () => {
-  if (euroinput.value !== "") {
+  if (euroinput.value !== "" && euroinput.value > 0) {
     const valor = euroinput.value;
 
     const resultado = valor * euro;
 
     resultadoInput.innerHTML = `<span class="text-success">Resultado: R$ ${resultado.toFixed(2)}</span>`;
-  } else if (dolarinput.value !== "") {
+  } else if (dolarinput.value !== "" && dolarinput.value > 0) {
     const valor = dolarinput.value;
 
     const resultado = valor * dolar;
